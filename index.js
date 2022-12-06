5b6bf38975abf159eb390ef5fa68e1b6992e3062
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-12-06 18:51:16 -0500

    commit tree f7f3031053db2a032f38507673dee238cbcf5463

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i=0; i<array.length; i++){
+    for (let j = i + 1; j < array.length; j++){
+        if((array[i] + array[j]) === target){
+          return true;
+        }
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Nested for statements ---> O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  PSUEDOCODE
+  FOR each number in an array (num1), add it to each subsequent number (num2), testing
+  whether num1 + num2 = target. IF num1 + num2 = target, return true, otherwise
+  keep testing. Need to have test run again if first number doesn't satisfy objective,
+  and so on for each following number until two numbers equal total, or no numbers equal total.
 */
 
 /*
-  Add written explanation of your solution here
+  Make a function that takes an array of numbers and adds two of them together to get a total.
+  Then test this total against the target number provided as an argument. 
+  [2, 5, 3, 4], target -> 9 .... test 2+5 = false, 2+3 = false, 2+4 = false, 2 is not one of the numbers.
+  test 5+3 (not 5+2 because 5+2 = 2+5 which we already tested)... false, 5+4 = true, no more tests!
+  How to test numbers without testing same number against itself? i.e. not 2+2, or 5+5, etc..
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+ 
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2,3,1], 4));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2,4,5], 7));
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([4,5,2], 7));
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +53,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5, 10, 3], 9));
 }
 
 module.exports = hasTargetSum;
